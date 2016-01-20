cca7d00f261552dacad73721dd80bfd643a6c3e7
Ville <k3023@student.jamk.fi>
2016-01-20 09:41:23 +0200

    Added a new project to solution.

---
+++ TestingApplication1/DEMO01_T4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO01_T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give Age:");
+            string line = Console.ReadLine();  // "50"
+            int age = int.Parse(line); // 50
+            if (age < 18)
+            {
+                Console.WriteLine("You are under-aged");
+            } else if (age >= 18 && age <= 65)
+            {
+                Console.WriteLine("You are adult");
+            } else
+            {
+                Console.WriteLine("You are senior.");
+            }
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
